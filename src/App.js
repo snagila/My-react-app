b0e3661a7abf90ccdac8797933b14bc9d82963ed
Sachin Nagila <sachinnagila2053@gmail.com>
2024-05-27 21:33:34 +1000

    1st component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Profile from "./UserComponent.jsx";
 
 const App = () => {
   return (
     <>
       <h1>hey hey</h1>
+      <hr />
+      <Profile />
     </>
   );
 };
---
+++ src/UserComponent.jsx
@@ -0,0 +1,8 @@
+const Profile = () => {
+  return (
+    <>
+      <div>Name:Sachin Nagila</div>
+    </>
+  );
+};
+export default Profile;
